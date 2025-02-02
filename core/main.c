0795a4790ac6c913b7be5287de839a303ec85590
Vladislav Prokopenko <vladislavprokopenko@disroot.org>
2025-02-02 17:52:51 +0200

    Started flushing the ps2 keyboard buffer

--- core/main.c
+++ core/main.c
@@ -5,6 +5,12 @@
 #include "../include/asm/ports-common.h"
 #include <stdbool.h>
 
+static inline void flush_ps2_buffer() {
+    while (inb(0x64) & 0x01) {
+        (void)inb(0x60);
+    }
+}
+
 void kernel_init() {
   print("[KERNEL_EARLY] Early Kernel loaded!\n");
   loadGDT();
@@ -13,6 +19,7 @@ void kernel_init() {
         inb(0x60); 
     }
   outb(0x64, 0xAE);
+  flush_ps2_buffer();
   asm volatile("sti");
   while (true) {
     asm("hlt");
